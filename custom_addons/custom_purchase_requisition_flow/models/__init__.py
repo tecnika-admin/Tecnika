b7427f13130fe8f8ea7cc19e49808b6e3fc55dbe
Jesús Adrián Garza Zavala <jesus.garza@tecnika.com.mx>
2025-04-17 21:06:31 -0700

    Se ajusta el código para resolver los2 errores

--- custom_addons/custom_purchase_requisition_flow/models/__init__.py
+++ custom_addons/custom_purchase_requisition_flow/models/__init__.py
@@ -2,5 +2,4 @@
 
 # CORREGIDO: Importar modelos base antes de los que dependen de ellos
 from . import purchase_requisition # Importar primero
-from . import account_move         # Importar segundo
 from . import purchase_requisition_line # Importar al final
