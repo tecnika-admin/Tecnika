beb649559fe9195fcb4e1f7dfcb2a89829c749f5
EberAngulo <eberchaidez@gmail.com>
2026-07-07 12:04:20 -0700

    [FIX]version 19:data import

--- custom_addons/nomina_cfdi_ee/__manifest__.py
+++ custom_addons/nomina_cfdi_ee/__manifest__.py
@@ -25,7 +25,7 @@
         'data/nomina.otropago.csv',
         'data/nomina.percepcion.csv',
         'data/nomina.deduccion.csv',
-        'data/hr_payroll_data.xml',
+        # 'data/hr_payroll_data.xml',
         #'data/hr_data.xml',
         'data/4.0/cfdi.xml',
         'views/hr_employee_view.xml',
