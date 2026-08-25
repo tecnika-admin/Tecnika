1f0b8bcd2e0a13a2872a80aa630f4e16d9f10494
EberAngulo <eberchaidez@gmail.com>
2026-08-24 19:22:22 -0700

    [FIX] nimina it admin 4e65b30

--- custom_addons/report_xlsx/controllers/main.py
+++ custom_addons/report_xlsx/controllers/main.py
@@ -42,7 +42,8 @@ class ReportController(ReportController):
             xlsxhttpheaders = [
                 (
                     "Content-Type",
-                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    "application/vnd.openxmlformats-"
+                    "officedocument.spreadsheetml.sheet",
                 ),
                 ("Content-Length", len(xlsx)),
             ]
@@ -50,7 +51,7 @@ class ReportController(ReportController):
         return super().report_routes(reportname, docids, converter, **data)
 
     @route()
-    def report_download(self, data, context=None, token=None, readonly=True):
+    def report_download(self, data, context=None, token=None):
         requestcontent = json.loads(data)
         url, report_type = requestcontent[0], requestcontent[1]
         if report_type == "xlsx":
@@ -103,6 +104,4 @@ class ReportController(ReportController):
                 error = {"code": 200, "message": "Odoo Server Error", "data": se}
                 return request.make_response(html_escape(json.dumps(error)))
         else:
-            return super().report_download(
-                data, context, token=token, readonly=readonly
-            )
+            return super().report_download(data, context=context, token=token)
